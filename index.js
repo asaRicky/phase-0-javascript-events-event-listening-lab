4df924507aeb387bd88ad67d34087febe4984abc
your name <your-email@foo.com>
2023-03-13 16:12:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+
+    const input = document.getElementById('input');
+input.addingEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+  const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+
